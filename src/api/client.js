bae5e60c041e2bf51f9da0c9f6557dc06e5d1d82
shubham <sonyshubhamm@gmail.com>
2026-08-11 00:09:03 +0530

    commit

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const apiBase = (import.meta.env.VITE_API_URL || '/api').replace(/\/$/, '');
+
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: apiBase,
 });
 
 api.interceptors.request.use((config) => {
