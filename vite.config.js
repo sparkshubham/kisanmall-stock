8a0a36008a2390483690950edc98f4a5a5f2bdb5
shubham <sonyshubhamm@gmail.com>
2026-08-10 22:00:18 +0530

    s

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,21 @@ import react from '@vitejs/plugin-react';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
+  appType: 'spa',
   plugins: [react(), basicSsl()],
   server: {
+    host: true,
+    port: 5173,
+    https: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  preview: {
     host: true,
     port: 5173,
     https: true,
